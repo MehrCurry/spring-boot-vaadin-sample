0d08f331dccc6d070be934eba9038e06cee25529
Guido Zockoll <gzockoll@gmail.com>
2015-02-28 10:10:07 +0100

    library updates

--- src/main/java/hello/MyVaadinUI.java
+++ src/main/java/hello/MyVaadinUI.java
@@ -1,19 +1,25 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.VaadinUI;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 
+import java.time.LocalTime;
+
 @VaadinUI
 public class MyVaadinUI extends UI {
 
     private static final long serialVersionUID = -5026493348790977491L;
 
+    @Autowired
+    private HelloController ctrl;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Hello! I'm the root UI!"));
+        setContent(new Label("Hello! I'm the root UI!" + ctrl.home() + " " + LocalTime.now() ));
     }
 
 }
