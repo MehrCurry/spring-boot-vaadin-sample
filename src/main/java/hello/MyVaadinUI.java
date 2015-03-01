14cef8d5c6870ff15dc49e45ea6804716a92529d
Guido Zockoll <gzockoll@gmail.com>
2015-03-01 09:51:53 +0100

    version updates

--- src/main/java/hello/MyVaadinUI.java
+++ src/main/java/hello/MyVaadinUI.java
@@ -1,11 +1,11 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.spring.VaadinUI;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import org.vaadin.spring.annotation.VaadinUI;
 
 import java.time.LocalTime;
 
